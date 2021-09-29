42fb9dc5e88b4c289b13bc609ee9093af173b5b2
Andres Diaz <adiazg@ieee.org>
2021-09-28 23:01:27 -0400

    change file structure

---
+++ notebooks/call_this_script.py
@@ -0,0 +1,7 @@
+'''
+Created by Victoria Catlett for the Intermediate Python 3 Workshop
+'''
+def call_this_function(x):
+    y = x**2
+    print('Wow, you called a function from another Python script!')
+    return y
